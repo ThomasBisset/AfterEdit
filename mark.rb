299dd8cefb731d8f59fb24dc7c3ef5740eabaf0e
Thomas Bisset <tombisset@gmail.com>
2013-10-13 17:09:28 +0100

    Now adds IPTC data

--- mark.rb
+++ mark.rb
@@ -8,8 +8,9 @@ require 'mini_exiftool'
 basedir = '/home/thomas/Ruby/'
 inputdir = 'INPUT/'
 outputdir = 'OUTPUT/'
-portrait = 'mark.png'
-landscape = 'mark.png'
+portrait = 'mark.png'	#	Watermark for portrait-oriented shots
+landscape = 'mark.png'	#	Watermark for landscape-oriented shots
+square = 'mark.png'		# 	Watermark for square shots
 
 photographer = 'Thomas Bisset'
 email = 'tom@thomasbisset.co.uk'
@@ -19,11 +20,23 @@ count = 0
 total = 0
 
 puts ''
-puts 'AfterEdit: Adding IPTC Data to images'
+puts 'Requesting Set Details ...'
+
+print 'Headline/Title?              >  '
+headline = gets.chomp
+print 'Caption?                     >  '
+caption = gets.chomp
+print 'Location?                    >  '
+location = gets.chomp
+print 'Keywords? (Comma Seperated)  >  '
+keywords = gets.chomp
+
+puts ''
+
 for file in Dir.glob(basedir+inputdir+'*.{jpg,jpeg,JPG,JPEG}')
 	total = Dir.glob(basedir+inputdir+'*.{jpg,jpeg,JPG,JPEG}').count
 	count = count + 1
-	puts 'Adding ITPC Information to image %d of %d...' % [count, total]
+	puts 'Adding ITPC Data to image %d of %d...' % [count, total]
 	data = MiniExiftool.new(file)
 	data["Author"] = photographer	
 	data["Creator"] = photographer
@@ -34,15 +47,23 @@ for file in Dir.glob(basedir+inputdir+'*.{jpg,jpeg,JPG,JPEG}')
 	data["CopyrightNotice"] = 'Copyright %s %s. All rights reserved.' % [photographer, Time.now.year]
 	data["CreatorWorkEmail"] = email
 	data["CreatorWorkURL"] = website
+	data["XPTitle"] = headline
+	data["Title"] = headline
+	data["Headline"] = headline
+	date = data["DateTimeOriginal"]
+	date = date.strftime('%A %e %B %Y').upcase			#	TO-DO: Add ordinals to string (1st, 2nd, 3rd, 4th etc)
+	data["Caption"] = '%s: %s (%s)' % [date, caption, location]
+	data["CaptionAbstract"] = '%s: %s (%s)' % [date, caption, location]
+	data["Location"] = location
+	data["Keywords"] = keywords
 	data.save
 end
-puts 'Standard IPTC information entry complete'
+
+puts 'IPTC data entry complete'
 puts ''
 
 count = 0
 total = 0
-
-puts 'AfterEdit: Starting Copy to '+basedir+outputdir
 
 for file in Dir.glob(basedir+inputdir+'*.{jpg,jpeg,JPG,JPEG}')
 	total = Dir.glob(basedir+inputdir+'*.{jpg,jpeg,JPG,JPEG}').count
@@ -50,13 +71,16 @@ for file in Dir.glob(basedir+inputdir+'*.{jpg,jpeg,JPG,JPEG}')
 	puts 'Copying file %d of %d...' % [count, total]
 	FileUtils.cp(file, basedir+outputdir)
 end
-puts 'Copy Complete. Originals files in '+basedir+inputdir
+
+puts 'Copy Complete' 
+puts ''
+puts 'Original files in '+basedir+inputdir
+puts 'Web ready files in '+basedir+outputdir
 puts ''
 
 count = 0
 total = 0
 
-puts 'AfterEdit: Adding Watermarks'
 for file in Dir.glob(basedir+outputdir+'*.{jpg,jpeg,JPG,JPEG}')
 	total = Dir.glob(basedir+outputdir+'*.{jpg,jpeg,JPG,JPEG}').count
 	count = count + 1
@@ -69,6 +93,11 @@ for file in Dir.glob(basedir+outputdir+'*.{jpg,jpeg,JPG,JPEG}')
 		result.write(file)
 	elsif img.rows < img.columns # If in landscape orientation
 		src = Magick::Image.read(landscape).first
+		img = img.resize_to_fit(1200, 1200)
+		result = img.composite(src, Magick::SouthEastGravity, Magick::OverCompositeOp)
+		result.write(file)
+	elsif img.rows == img.columns # If square crop
+		src = Magick::Image.read(square).first
 		img = img.resize_to_fit(1200, 1200)
 		result = img.composite(src, Magick::SouthEastGravity, Magick::OverCompositeOp)
 		result.write(file)
